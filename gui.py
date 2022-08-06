5e3d9aed965297bf47e063267d534bb78ca9b4be
TalShlamovich <talshlamovich@gmail.com>
2022-08-06 11:41:19 +0300

    first commit

---
+++ gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('600x300')
+root.resizable(0,0)
+root. title("StudyGyaan - Website Blocker")
+
+Label(root, text ='WEBSITE BLOCKER' , font ='arial 20 bold').pack() 
+Label(root, text ='StudyGyaan' , font ='arial 20 bold').pack(side=BOTTOM)
