6712137fbc20abcd236412e92db3113cf74cd697
TalShlamovich <talshlamovich@gmail.com>
2022-08-07 20:50:47 +0300

    final version

--- interface.py
+++ interface.py
@@ -6,40 +6,68 @@ ip_address = '127.0.0.1'
 
 
 
+
+
 def block() -> None:
-    
-    website_list = text_box.get(1.0,END)
-    website = list(website_list.split(","))
+    """Converts user input from the text box into an array (for cases when user inputs a few websites simultaneously)
+    Then opens hosts file, checks whether the website is already there 
+    If not then writes it into the file
+    When commad is executed creates a message for a user"""
+    input_website = text_box.get(1.0,END)
+    website = list(input_website.split(", "))
+
     with open (host_path , 'r+') as host_file:
         file_content = host_file.read()
+
         for address in website:
             if address in file_content:
-                display=Label(gui, text = 'You already blocked this website' , font = 'helvetica')
-                display.place(x=200,y=200)
+                message=Label(gui, text = 'You already blocked this website' , font = 'Helvetica')
+                message.place(x=310, y= 390, anchor='center')
+                message.after(1500, message.destroy)
                 pass
+
             else:
                 host_file.write(ip_address + " " + address + '\n')
-                Label(gui, text = "Blocked", font = 'arial').place(x=230,y =200)
+                message = Label(gui, text = "Blocked", font = 'Helvetica')
+                message.place(x=310, y= 390, anchor='center')
+                message.after(1500, message.destroy)
 
 
-
+        
+         
+            
 def unblock() -> None:
-    
-    website_list = text_box.get(1.0,END)
-    website = list(website_list.split(","))
-    with open (host_path , 'r+') as host_file:
-        file_content = host_file.readlines()
-    for web in website:
-            if web in website_list:
-                with open (host_path , 'r+') as f:
-                    for line in file_content:
-                        if line.strip(',') != website_list:
-                            f.write(line)
-                            Label(gui, text = "Unblocked", font = 'arial').place(x=350,y =200)
-                            pass
-                        else:
-                            display=Label(gui, text = 'This website is not currently blocked' , font = 'arial')
-                            display.place(x=350,y=200)
+    """Take the user input and convert it to a list.
+    Then opens hosts file, checks whether the website is already there
+    Then rewrite the file line by line excluding the line that has user input
+    When commad is executed creates a message for a user"""
+
+    input_website = text_box.get(1.0,END)
+    website = list(input_website.split(", "))
+    website[-1]= website[-1][0:-1]
+
+
+    with open (host_path , 'r') as host_file: 
+        file_content = host_file.readlines()    
+    for address in website:
+        print (address)
+        if (ip_address + " " + address + '\n') in file_content:
+            with open (host_path , 'w') as f:
+                for line in file_content:
+                    print(line)
+                    if address not in line.strip(','):
+                        f.write(line)
+                        message = Label(gui, text = "Unblocked", font = 'Helvetica')
+                        message.place(x=310, y=390, anchor='center')
+                        message.after(1500, message.destroy)
+                        
+                        
+        else:
+            message = Label(gui, text = 'This website is not currently blocked' , font = 'Helvetica')
+            message.place(x=310, y= 390, anchor='center')
+            message.after(1500, message.destroy)
+
+
 
 # Creating GUI
 
@@ -48,17 +76,22 @@ gui.geometry('600x400')
 gui.resizable(0,0)
 gui.title("Website Blocker by Tal")
 
-text_box_label = Label(gui, text = "Enter the website address", font = 'helvetica')
+text_box_label = Label(gui, text = "Enter the website address", font = 'Helvetica')
 text_box_label.place(x=300, y=60, anchor ='center')
 
-text_box = Text(gui, font = 'helvetica', height = '2', width = '30')
+text_box = Text(gui, font = 'Helvetica', height = '1', width = '30')
 text_box.place(x=300, y=110, anchor = 'center')
 
 
-blocking_button = Button(gui, text = 'Block me!',font = 'arial',pady = 5,command = block ,width = 10, bg = 'red', activebackground = 'grey')
+blocking_button = Button(gui, text = 'Block me!',font = 'Helvetica',pady = 5,command = block ,width = 10, bg = 'red', activebackground = 'grey')
 blocking_button.place(x = 250, y = 200, anchor='center')
 
-unblock_button = Button(gui, text = 'Set me free!',font = 'arial',pady = 5,command = unblock ,width = 10, bg = 'green', activebackground = 'grey')
+unblock_button = Button(gui, text = 'Set me free!',font = 'Helvetica',pady = 5,command = unblock ,width = 10, bg = 'green', activebackground = 'grey')
 unblock_button.place(x = 370, y = 200, anchor='center')
 
+
 gui.mainloop()
+
+
+
+
